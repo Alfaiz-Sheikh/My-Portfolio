f17b3c37223c6309e5c65299e4eb6e7a2cc6402d
Alfaiz Sheikh <alfaizhsheikh1054@gmail.com>
2025-08-19 12:07:36 +0530

    Header Section Completed

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+const more = document.querySelector(".more");
+const list = document.querySelector("#list");
+
+const showMenu = () => {
+    if(list.style.display == "none"){
+        list.style.display = "block";
+    }else{
+        list.style.display = "none";
+    }
+}
+
+more.addEventListener("click", showMenu);
